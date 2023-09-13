7f2fe63a64d8687014e8ca2580fccd89f6974022
Pramod9890 <pramodhatture@gmail.com>
2023-09-13 18:36:12 +0530

    react_spring_dotnet

--- BackEnd/JAVA Spring/demo/src/main/java/com/example/repositories/PaymentTypeMasterRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.example.repositoryPayment;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.example.entityPayment.PaymentTypeMaster;
-//
-//public interface PaymentTypeMasterRepository extends JpaRepository<PaymentTypeMaster, Integer> {
-//
-//}
